4e282160941ec2a36d55ece8c0391f8c11c3f71d
sabrina <sabrina.liewxintong@gmail.com>
2023-06-09 22:36:24 +0800

    login-notdone

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,69 +1,88 @@
 import React, { useState } from "react";
-import { SafeAreaView , TextInput, Pressable, Text, Alert} from "react-native";
+import { SafeAreaView, TextInput, Pressable, Text, View, Alert } from "react-native";
 import LoginButton from "../components/LoginButton";
 import globalStyles from "../styles/globalStyles";
-import {getAuth,signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth"
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth"
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import app from "../api/fireabaseConfig";
 
 
 const Stack = createNativeStackNavigator()
 
-const UserLogin = ({navigation}) => {
+const UserLogin = ({ navigation }) => {
     const auth = getAuth()
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-    
-    return(
-        <SafeAreaView> 
-            <TextInput placeholder = "Email" onChangeText={(e)=>{setEmail(e)}}/>
-            <TextInput placeholder="Password" onChangeText= {(e)=>(setPassword(e))}/>
-            <Pressable onPress={()=>{navigation.navigate('userRegister')}}><Text>New Here? Register.</Text></Pressable>
-            <Pressable onPress={()=>{
-                console.log(email,password)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-                signInWithEmailAndPassword(auth,email,password)
-                .then((userCredential)=>{
-                    const user = userCredential.user
-                    console.log(user)
-                })
-                .catch((error)=>{
-                    console.log(error.code)
-                    console.log(error.message)
-                })
+    return (
+        <SafeAreaView style={globalStyles.container}>
 
-            }} 
-            style = {globalStyles.LoginButton}>
-                <Text> Login </Text>
-            </Pressable>
+            <Text style={globalStyles.login_Title}>Login</Text>
+
+            <View style={globalStyles.login_Container}>
+
+                <TextInput style={globalStyles.userInput_email}
+                    placeholder="Email"
+                    placeholderTextColor="#818589"
+                    onChangeText={(e) => { setEmail(e) }} />
+
+                <TextInput style={globalStyles.userInput_password}
+                    placeholder="Password"
+                    placeholderTextColor="#818589"
+                    onChangeText={(e) => (setPassword(e))} />
+
+                <Pressable onPress={() => {
+                    console.log(email, password)
+
+                    signInWithEmailAndPassword(auth, email, password)
+                        .then((userCredential) => {
+                            const user = userCredential.user
+                            console.log(user)
+                        })
+                        .catch((error) => {
+                            console.log(error.code)
+                            console.log(error.message)
+                        })
+
+                }}
+                    style={globalStyles.LoginButton}>
+                    <Text style={globalStyles.LoginText}> Login </Text>
+                </Pressable>
+            </View>
+            <Text>Don't have an account?</Text>
+
+            <Pressable onPress={() => { navigation.navigate('userRegister') }}
+                style={globalStyles.RegisterButton}>
+                <Text style={globalStyles.GetStartedText}>Sign up</Text></Pressable>
+
         </SafeAreaView>
     )
 }
-const UserSignup = ({navigation}) =>{
+const UserSignup = ({ navigation }) => {
     const auth = getAuth()
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    return(
+    return (
         <SafeAreaView>
-            <TextInput placeholder = "Email" onChangeText={(e)=>{setEmail(e)}}/>
-            <TextInput placeholder="Password" onChangeText= {(e)=>(setPassword(e))}/>
-            <Pressable onPress={()=>{navigation.navigate('userLogin')}}><Text>Already a User?</Text></Pressable>
-            <Pressable onPress={()=>{
-                console.log(email,password)
+            <TextInput placeholder="Email" onChangeText={(e) => { setEmail(e) }} />
+            <TextInput placeholder="Password" onChangeText={(e) => (setPassword(e))} />
+            <Pressable onPress={() => { navigation.navigate('userLogin') }}><Text>Already a User?</Text></Pressable>
+            <Pressable onPress={() => {
+                console.log(email, password)
 
-                createUserWithEmailAndPassword(auth,email,password)
-                .then(()=>{
-                   alert('Account Created!' , "Please Log In!")
-                    navigation.navigate('userLogin')
-                })
-                .catch((error)=>{
-                    console.log(error.code)
-                    console.log(error.message)
-                })
+                createUserWithEmailAndPassword(auth, email, password)
+                    .then(() => {
+                        alert('Account Created!', "Please Log In!")
+                        navigation.navigate('userLogin')
+                    })
+                    .catch((error) => {
+                        console.log(error.code)
+                        console.log(error.message)
+                    })
 
-            }} 
-            style = {globalStyles.LoginButton}>
+            }}
+                style={globalStyles.LoginButton}>
                 <Text> Register </Text>
             </Pressable>
         </SafeAreaView>
@@ -76,11 +95,11 @@ const UserSignup = ({navigation}) =>{
 
 }
 const LoginStack = () => {
-    return(
-    <Stack.Navigator>
-        <Stack.Screen name = 'userLogin' component ={UserLogin} options = {{headerShown : false}}/>
-        <Stack.Screen name = 'userRegister' component = {UserSignup} options = {{headerShown: false}}/>
-    </Stack.Navigator>
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='userLogin' component={UserLogin} options={{ headerShown: false }} />
+            <Stack.Screen name='userRegister' component={UserSignup} options={{ headerShown: false }} />
+        </Stack.Navigator>
     )
 
 }
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -5,44 +5,96 @@ const globalStyles = StyleSheet.create({
     
     container:{
         flex:1,
-        BackgroundColor:'white'
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 3000,
+        BackgroundColor:'#FFFFFF'
+    },
+    
+    login_Container:{
+        marginBottom: 10,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        borderColor: '#CED0ED',
+        height: 400,
+        width: 320,
+        shadowColor: 'rgba(0, 0, 0, 0.1)',
+        shadowOpacity: 1.8,
+        shadowRadius: 15 ,
+        shadowOffset : { width: 1, height: 13},
+
+    },
+    userInput_email: {
+        backgroundColor: 'white',
+        borderRadius: 20,
+        marginBottom: 20,
+        height: 45,
+        width: 255,
+        fontSize: 16,
+        paddingLeft: 20,
+        shadowColor: 'rgba(0, 0, 0, 0.1)',
+        shadowOpacity: 1.8,
+        shadowRadius: 15 ,
+        shadowOffset : { width: 1, height: 13},
+    },
+    login_Title:{
+        fontSize: 30,
+        color: '#6966FF',
+        fontWeight: 'bold',
+        marginBottom: 30,
+        marginRight: 225,
+    
     },
 
+
     // Same button styles for Sign Up button in Sign up page
-    LoginButton:{
-        text: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
+    LoginButton:{ 
         alignItems: 'center',
         justifyContent: 'center',
+        height: 55,
+        width: 270,
+        paddingTop: 5,
+        paddingBottom: 5,
         backgroundColor: '#6966FF',
-        maxWidth: 282,
-        minHeight: 50,
         borderRadius: 25,
-        marginBottom: 20,
-        marginLeft: 50,
-        marginRight: 50,
         shadowColor: '#6966FF',
         shadowOpacity: 0.5,
-        shadowOffset: {width: 2, height: 4},
+        shadowRadius: 15,
+        shadowOffset: {width: 1, height: 13},
+    },
+    LoginText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
     },
     // Sign up button in Login Page
-    WhiteButton: {
-        text: '#6966FF',
-        fontSize: 20,
-        fontWeight: 'bold',
+    RegisterButton: {
         alignItems: 'center',
         justifyContent: 'center',
+        height: 55,
+        width: 270,
+        marginTop: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
         backgroundColor: '#FFFFFF',
-        maxWidth: 282,
-        minHeight: 50,
         borderRadius: 25,
-        marginLeft: 50,
-        marginRight: 50,
         shadowColor: '#6966FF',
         shadowOpacity: 0.5,
-        shadowOffset: {width: 2, height: 4},
-    }
+        shadowRadius: 15,
+        shadowOffset: {width: 1, height: 13},
+    },
+    GetStartedText: {
+        color: '#6966FF',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    userInput_email:{
+        
+
+
+    },
 
 })
 export default globalStyles
