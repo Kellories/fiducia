08cba7fe060e247b623cf54373a13ccd5cd1bcd6
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-06-23 22:28:22 +0800

    feat: More font and layout changes

---
+++ src/components/backButton.jsx
@@ -0,0 +1,23 @@
+import { View } from "react-native";
+import React from 'react'
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const BackButton = ({onPress}) => {
+    return(
+        <View style = {{marginLeft:10,marginTop:20,maxWidth:36,}} >
+            <MaterialCommunityIcons.Button
+                name = "arrow-left"
+                color={"#202020"}
+                size={20}
+                backgroundColor={"white"}
+                iconStyle={{marginRight: 0,marginLeft:0}}
+                borderRadius = {100}
+                padding = {8}
+                onPress = {onPress}
+            >
+            </MaterialCommunityIcons.Button>
+        
+        </View>
+    )
+}
+export default BackButton;
